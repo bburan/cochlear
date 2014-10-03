633a83a2d86a093b2f4a403a33dcb3ef8a78a312
Brad Buran <buran@ohsu.edu>
2014-10-03 07:46:19 -0700

    ENH: Added setup/license files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Cochlear',
+    version='0.1',
+    author='Brad Buran (bburan@alum.mit.edu)',
+    packages=['cochlear'],
+    url='http://github.com/bburan/cochlear',
+    license='LICENSE.txt',
+    description='Module for various auditory experiments',
+    requires=['numpy'],
+)
