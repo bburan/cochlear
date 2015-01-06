e1d4c36afab151f46752cbbcd203928bf419dea4
Brad Buran <buran@ohsu.edu>
2015-01-06 15:45:47 -0800

    Combined calibration_chirp files

--- cochlear/calibration_chirp.py
+++ cochlear/calibration_chirp.py
@@ -1,5 +1,22 @@
 from __future__ import division
 
+import shutil
+
+import numpy as np
+
+from traits.api import (Float, Int, Property, Any, Bool, Enum)
+from traitsui.api import View, Item, VGroup, Include
+from enable.api import Component, ComponentEditor
+from chaco.api import (DataRange1D, VPlotContainer, PlotAxis, create_line_plot,
+                       LogMapper, OverlayPlotContainer)
+
+from experiment import (icon_dir, AbstractController, AbstractParadigm)
+from experiment.util import get_save_file
+
+from neurogen import block_definitions as blocks
+from neurogen.calibration import InterpCalibration
+from neurogen.util import patodb, db
+
 import os.path
 import shutil
 
@@ -19,20 +36,62 @@ from experiment.util import get_save_file
 
 from neurogen.util import db, dbi
 
-#import calibration_standard as standard
-from base_calibration_chirp import BaseChirpCalSettings, BaseChirpCalController
+from cochlear import nidaqmx as ni
+from cochlear import calibration_standard as reference_calibration
 import settings
 
+DAC_FS = 200e3
+ADC_FS = 200e3
 
-class ReferenceChirpCalSettings(BaseChirpCalSettings):
+
+def get_chirp_transform(vrms):
+    calibration_data = np.array([
+        (1, 30),
+        (100e3, -10),
+    ])
+    frequencies = calibration_data[:, 0]
+    magnitude = calibration_data[:, 1]
+    return InterpCalibration.from_single_vrms(frequencies, magnitude, vrms)
+
+
+class ChirpCalSettings(AbstractParadigm):
 
     kw = dict(context=True)
-    ref_mic_sens_mv = Float(2.685, label='Ref. mic. sens. (mV/Pa)', **kw)
+
+    output = Enum(('ao1', 'ao0'), label='Analog output (channel)', **kw)
+    rise_time = Float(5e-3, label='Envelope rise time', **kw)
+    amplitude = Float(1, label='Waveform amplitude (Vrms)', **kw)
+    output_gain = Float(6, label='Output gain (dB)', **kw)
+    freq_lb = Float(0.1e3, label='Start frequency (Hz)', **kw)
+    freq_ub = Float(60e3, label='End frequency (Hz)', **kw)
+    freq_resolution = Float(50, label='Frequency resolution (Hz)')
+    fft_averages = Int(32, label='Number of FFTs', **kw)
+    waveform_averages = Int(32, label='Number of chirps per FFT', **kw)
+    ici = Float(0.01, label='Inter-chirp interval', **kw)
+
+    ref_mic_sens_mv = Float(2.703, label='Ref. mic. sens. (mV/Pa)', **kw)
     ref_mic_gain = Float(0, label='Ref. mic. gain (dB)', **kw)
     ref_mic_sens = Property(depends_on='ref_mic_sens_mv', **kw)
+    exp_mic_gain = Float(40, label='Exp. mic. gain (dB)', **kw)
+
+    averages = Property(depends_on='fft_averages, waveform_averages',
+                        label='Number of chirps', **kw)
+    duration = Property(depends_on='freq_resolution',
+                        label='Chirp duration (sec)', **kw)
+    total_duration = Property(depends_on='duration, averages, ici',
+                              label='Total duration (sec)')
 
     def _get_ref_mic_sens(self):
         return self.ref_mic_sens_mv*1e-3
+    def _get_duration(self):
+        return 1/self.freq_resolution
+
+    def _get_total_duration(self):
+        return (self.duration+self.ici)*self.averages
+
+    def _get_averages(self):
+        return self.fft_averages*self.waveform_averages
+
 
     output_settings = VGroup(
         'output',
@@ -50,8 +109,31 @@ class ReferenceChirpCalSettings(BaseChirpCalSettings):
         show_border=True,
     )
 
+    stimulus_settings = VGroup(
+        'rise_time',
+        'freq_lb',
+        'freq_ub',
+        'freq_resolution',
+        'fft_averages',
+        'waveform_averages',
+        'ici',
+        Item('averages', style='readonly'),
+        Item('duration', style='readonly'),
+        Item('total_duration', style='readonly'),
+        show_border=True,
+        label='Chirp settings',
+    )
 
-class ReferenceChirpCalData(AbstractData):
+    traits_view = View(
+        VGroup(
+            Include('output_settings'),
+            Include('mic_settings'),
+            Include('stimulus_settings'),
+        )
+    )
+
+
+class ChirpCalData(AbstractData):
 
     exp_microphone = Instance('experiment.channel.EpochChannel')
     ref_microphone = Instance('experiment.channel.EpochChannel')
@@ -135,52 +217,10 @@ class ReferenceChirpCalData(AbstractData):
         self._create_array('waveform', self.waveform)
 
 
-class ReferenceChirpCalController(BaseChirpCalController):
+class ChirpCal(HasTraits):
 
-    MIC_INPUT = '{}, {}'.format(BaseChirpCalController.MIC_INPUT,
-                                BaseChirpCalController.ERP_INPUT)
-    filename = Any
-
-    def save(self, info=None):
-        filename = get_save_file(settings.CALIBRATION_DIR,
-                                 'Microphone calibration|*.mic')
-        if filename is not None:
-            # Ensure all data is written to file before we copy it over
-            self.model.data.fh.flush()
-            shutil.copy(self.filename, filename)
-
-    def poll(self, waveform):
-        self.model.data.exp_microphone.send(waveform[:, 0, :])
-        self.model.data.ref_microphone.send(waveform[:, 1, :])
-        self.epochs_acquired += 1
-        if self.epochs_acquired == self.get_current_value('averages'):
-            self.finalize()
-
-    def finalize(self):
-        waveform_fs, waveform = \
-            self.iface_dac.fs, self.iface_dac.realize().ravel()
-
-        ref_mic_sens = self.get_current_value('ref_mic_sens')
-        ref_mic_gain = self.get_current_value('ref_mic_gain')
-        exp_mic_gain = self.get_current_value('exp_mic_gain')
-        waveform_averages = self.get_current_value('waveform_averages')
-        self.model.data.compute_transfer_functions(waveform_fs, waveform,
-                                                   ref_mic_sens, ref_mic_gain,
-                                                   exp_mic_gain,
-                                                   waveform_averages)
-        self.model.data.save(**dict(self.model.paradigm.items()))
-        self.model.generate_plots()
-        self.complete = True
-        self.stop()
-
-    #def run_standard_calibration(self, info):
-    #    standard.launch_gui(parent=info.ui.control, kind='livemodal')
-
-
-class ReferenceChirpCal(HasTraits):
-
-    paradigm = Instance(ReferenceChirpCalSettings, ())
-    data = Instance(ReferenceChirpCalData)
+    paradigm = Instance(ChirpCalSettings, ())
+    data = Instance(ChirpCalData)
     container = Instance(Component)
 
     def generate_plots(self):
@@ -274,7 +314,7 @@ class ReferenceChirpCal(HasTraits):
         ),
         toolbar=ToolBar(
             '-',
-            Action(name='Ref. cal.', action='start',
+            Action(name='Ref. cal.', action='run_reference_calibration',
                    image=ImageResource('tool', icon_dir),
                    enabled_when='not handler.state=="running"'),
             '-',
@@ -293,22 +333,147 @@ class ReferenceChirpCal(HasTraits):
     )
 
 
+class ChirpCalController(AbstractController):
+
+    adc_fs = ADC_FS
+    dac_fs = DAC_FS
+    epochs_acquired = Int(0)
+    complete = Bool(False)
+    fh = Any(None)
+    filename = Any
+
+    def save(self, info=None):
+        filename = get_save_file(settings.CALIBRATION_DIR,
+                                 'Microphone calibration|*.mic')
+        if filename is not None:
+            # Ensure all data is written to file before we copy it over
+            self.model.data.fh.flush()
+            shutil.copy(self.filename, filename)
+
+    def start(self, info=None):
+        self.complete = False
+        self.state = 'running'
+        self.epochs_acquired = 0
+        self.complete = False
+        self.calibration_accepted = False
+        if self.fh is not None:
+            self.fh.close()
+
+        self.initialize_context()
+        self.refresh_context()
+        self._setup_input()
+        self._setup_output()
+        self.iface_dac.play_queue()
+
+    def stop(self, info=None):
+        self.state = 'halted'
+        self.iface_dac.clear()
+        self.iface_adc.clear()
+        self.complete = True
+
+    def poll(self, waveform):
+        self.model.data.exp_microphone.send(waveform[:, 0, :])
+        self.model.data.ref_microphone.send(waveform[:, 1, :])
+        self.epochs_acquired += 1
+        if self.epochs_acquired == self.get_current_value('averages'):
+            self.finalize()
+
+    def _setup_input(self):
+        mic_input = '{}, {}'.format(ni.DAQmxDefaults.MIC_INPUT,
+                                    ni.DAQmxDefaults.REF_MIC_INPUT)
+        epoch_duration = self.get_current_value('duration')
+        self.iface_adc = ni.TriggeredDAQmxSource(
+            fs=self.adc_fs, epoch_duration=epoch_duration, input_line=mic_input,
+            callback=self.poll, trigger_duration=10e-3, expected_range=1.0)
+        self.model.data._create_microphone_nodes(self.adc_fs, epoch_duration)
+        self.iface_adc.setup()
+        self.iface_adc.start()
+
+    def _setup_output(self, output=None):
+        if output is None:
+            output = self.get_current_value('output')
+        freq_lb = self.get_current_value('freq_lb')
+        freq_ub = self.get_current_value('freq_ub')
+        epoch_duration = self.get_current_value('duration')
+        ici = self.get_current_value('ici')
+        averages = self.get_current_value('averages')
+        output_gain = self.get_current_value('output_gain')
+        vrms = self.get_current_value('amplitude')
+        rise_time = self.get_current_value('rise_time')
+        calibration = get_chirp_transform(vrms)
+        analog_output = '/{}/{}'.format(ni.DAQmxDefaults.DEV, output)
+
+        self.iface_dac = ni.QueuedDAQmxPlayer(
+            fs=self.dac_fs, output_line=analog_output, duration=epoch_duration)
+
+        # By using an Attenuation calibration and setting tone level to 0, a
+        # sine wave at the given amplitude (as specified in the settings) will
+        # be generated at each frequency as the reference.
+        ramp = blocks.LinearRamp(name='sweep')
+        self.current_channel = \
+            blocks.Tone(name='tone', level=0, frequency=ramp) >> \
+            blocks.Cos2Envelope(name='envelope') >> \
+            ni.DAQmxChannel(calibration=calibration)
+        self.current_channel.set_value('sweep.ramp_duration', epoch_duration)
+        self.current_channel.set_value('envelope.duration', epoch_duration)
+        self.current_channel.set_value('envelope.rise_time', rise_time)
+        self.current_channel.set_value('sweep.start', freq_lb)
+        self.current_channel.set_value('sweep.stop', freq_ub)
+
+        self.iface_dac.add_channel(self.current_channel)
+        self.iface_dac.queue_init('FIFO')
+        self.iface_dac.queue_append(averages, ici)
+
+        self.iface_att = ni.DAQmxAttenControl()
+        self.iface_att.setup()
+        if output == 'ao0':
+            self.iface_att.set_gain(-np.inf, output_gain)
+        elif output == 'ao1':
+            self.iface_att.set_gain(output_gain, -np.inf)
+        self.iface_att.set_mute(False)
+
+    def finalize(self):
+        waveform_fs, waveform = \
+            self.iface_dac.fs, self.iface_dac.realize().ravel()
+
+        ref_mic_sens = self.get_current_value('ref_mic_sens')
+        ref_mic_gain = self.get_current_value('ref_mic_gain')
+        exp_mic_gain = self.get_current_value('exp_mic_gain')
+        waveform_averages = self.get_current_value('waveform_averages')
+        self.model.data.compute_transfer_functions(waveform_fs, waveform,
+                                                   ref_mic_sens, ref_mic_gain,
+                                                   exp_mic_gain,
+                                                   waveform_averages)
+        self.model.data.save(**dict(self.model.paradigm.items()))
+        self.model.generate_plots()
+        self.complete = True
+        self.stop()
+
+    def run_reference_calibration(self, info):
+        reference_calibration.launch_gui(parent=info.ui.control,
+                                         kind='livemodal')
+
+
 def launch_gui(output='ao0', **kwargs):
     tempfile = os.path.join(settings.TEMP_DIR, 'temp_mic.cal')
     with tables.open_file(tempfile, 'w') as fh:
-        data = ReferenceChirpCalData(store_node=fh.root)
-        controller = ReferenceChirpCalController(filename=tempfile)
-        paradigm = ReferenceChirpCalSettings(output=output)
-        ReferenceChirpCal(data=data, paradigm=paradigm) \
+        data = ChirpCalData(store_node=fh.root)
+        controller = ChirpCalController(filename=tempfile)
+        paradigm = ChirpCalSettings(output=output)
+        ChirpCal(data=data, paradigm=paradigm) \
             .edit_traits(handler=controller, **kwargs)
 
 
 if __name__ == '__main__':
     output = 'ao0'
     tempfile = os.path.join(settings.TEMP_DIR, 'temp_mic.cal')
+    #from cochlear import configure_logging
+    #configure_logging('temp.log')
+    import logging
+    logging.basicConfig(level='DEBUG')
     with tables.open_file(tempfile, 'w') as fh:
-        data = ReferenceChirpCalData(store_node=fh.root)
-        controller = ReferenceChirpCalController(filename=tempfile)
-        paradigm = ReferenceChirpCalSettings(output=output)
-        ReferenceChirpCal(data=data, paradigm=paradigm) \
+        data = ChirpCalData(store_node=fh.root)
+        controller = ChirpCalController(filename=tempfile)
+        paradigm = ChirpCalSettings(output=output)
+        ChirpCal(data=data, paradigm=paradigm) \
             .configure_traits(handler=controller)
